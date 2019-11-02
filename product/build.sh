f9e83767cc70b32f71d6e66652e3f6eb309c76f4
abi <495118772@qq.com>
2019-11-02 22:19:50 +0800

    网关

--- product/build.sh
+++ product/build.sh
@@ -3,7 +3,7 @@
 mvn clean package -Dmaven.test.skip=true -U
 
 # docker打包
-docker build -t registry.cn-hangzhou.aliyuncs.com/shoubi/springcloud:product .
+docker build -t registry.cn-hangzhou.aliyuncs.com/shoubi/product:latest .
 
 # docker push到阿里云镜像仓库
-docker push registry.cn-hangzhou.aliyuncs.com/shoubi/springcloud:product
+docker push registry.cn-hangzhou.aliyuncs.com/shoubi/product:latest
